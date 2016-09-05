b4dccae5a720c144de859ec7145c41e369d72d53
Clase <Clase@FDELAB-02>
2016-09-05 13:28:32 -0500

    Hola TravisCI....

--- src/traviscimafer/TravisCIMafer.java
+++ src/traviscimafer/TravisCIMafer.java
@@ -14,6 +14,7 @@ public class TravisCIMafer {
         System.out.println("Calculadora Básica");
         System.out.println("Ingeniería de Software");
         System.out.println("Ing. Diego Toala");
+        System.out.println("María Fernanda");
         int op=0;
         double n1,n2,multiplicacion,suma,division,resta;
         do{
